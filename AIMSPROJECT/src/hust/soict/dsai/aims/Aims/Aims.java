49ff51e96366f1a8f45c132806fb5e23c0a534a7
vinhdv2k4 <laivinh2906@gmail.com>
2024-12-03 22:46:36 +0700

    final

--- AIMSPROJECT/src/hust/soict/dsai/aims/Aims/Aims.java
+++ AIMSPROJECT/src/hust/soict/dsai/aims/Aims/Aims.java
@@ -282,14 +282,16 @@ public class Aims {
 
     public static void cartMenu() {
         System.out.println("Options: ");
-        System.out.println("--------------------------------");
-        System.out.println("1. Filter medias in cart");
-        System.out.println("2. Sort medias in cart");
-        System.out.println("3. Remove media from cart");
-        System.out.println("4. Play a media");
-        System.out.println("0. Back");
-        System.out.println("--------------------------------");
-        System.out.println("Please choose a number: 0-1-2-3-4-5");
+		System.out.println("--------------------------------");
+		System.out.println("1. Filter medias in cart");
+		System.out.println("2. Sort medias in cart");
+		System.out.println("3. Remove media from cart");
+		System.out.println("4. Play a media");
+		System.out.println("5. Place order");
+		System.out.println("0. Back");
+		System.out.println("--------------------------------");
+		System.out.println("Please choose a number: 0-1-2-3-4-5");
+
     }
 
     //storeMenu - 1
